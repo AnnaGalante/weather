589fffe3ff358395032d2275913d85f228d13bda
Anna G <Anna@Annastasias-MacBook-Pro.local>
2017-06-06 17:23:03 -0400

    Where is the response?
    http request, gotta figure out the response

--- javascript.js
+++ javascript.js
@@ -1,9 +1,13 @@
-function getWeather(){
-	//get Location   ////////////////
+//useful variables
+	var xhr = new XMLHttpRequest();
 	var locationOutput = document.getElementById("locationTest");
-
+	var city = document.getElementById("currentCity");
+	var state = document.getElementById("currentState");
+	var currentTempDisplay = document.getElementById("currentTempDisplay");
+function getLocation(){
+	//get Location   ////////////////
 	var options = {
-		//enableHighAccuracy: true,// enabling increases load time
+		//enableHighAccuracy: true, //increases load time
 		timeout: 8000,
 		maximumAge: 0
 	};
@@ -12,22 +16,30 @@ function getWeather(){
 		locationOutput.textContent = "Lat: " + crd.latitude + ", Long: " + crd.longitude;
 	};
 	function error (err) {
-		locationOutput.textContent = "This isn't working";
+		locationOutput.textContent = "Error. Please Enable ";
 	};
 
-	navigator.geolocation.getCurrentPosition(success, error, options);
+	navigator.geolocation.getCurrentPosition(success, error, options) //get location
+};
 
-	//Load API ////////////////
- 
+function getWeather(){
+	//Set up API (variables, lat/lon, key, and api URL its self) ////////////////
+	var latlon = crd.latitude + "," + crd.longitude;
+	var darkSkyKey = "0a8032c051266de6bab2af3789f66756"; //obscure this later
+	var darkSkyAPI = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyKey + "/" + latlon;
 
-	//Get weather for location  ///////////////// 
+ 	//Parse through JSON File///////////////////////
+ 	var darkSkyResponse = xhr.response;
 
-	//Define values   //////////////
-	var youAreHere = "City success!";
-	var yourState = "State Success";
-	var currentTemp = "60"
-	//Plug in values  //////////////
-	document.getElementById("currentCity").textContent = youAreHere;
-	document.getElementById("currentState").textContent = yourState;
-	document.getElementById("currentTempDisplay").textContent = currentTemp;
+ 	//var fetchedTemp = xhr[currently[temperature]]darkSkyResponse;
+ 	//this is breaking shit for some reason so its commented out
+ 	
+ 	//Pop into page ////////////////////////
+
+ 	//currentTempDisplay.textContent = fetchedTemp;
+ 	//Call the API ///////////////////////////////////////////
+ 	xhr.open('GET', 'darkSkyAPI'); //gets the response
+ 	xhr.withCredentials = true; //tells server that creditials are needed?
+ 	xhr.send(null); //not POST, just GET, so send is empty
+	
 };
